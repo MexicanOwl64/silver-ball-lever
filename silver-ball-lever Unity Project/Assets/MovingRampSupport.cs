d25346617556791ebc13b211034c7be341635c0c
MexicanOwl64 <55261799+MexicanOwl64@users.noreply.github.com>
2019-10-03 18:10:55 -0500

    Worked on the Game
    Succeded on making the square move

---
+++ silver-ball-lever Unity Project/Assets/MovingRampSupport.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingRampSupport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello WOrld");
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 5, 0);
+        }
+        Debug.Log("UPDATE METHOD WHy");
+    }
+}
