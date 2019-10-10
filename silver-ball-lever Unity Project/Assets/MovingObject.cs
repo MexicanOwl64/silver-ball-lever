818b00f385ed2c6f5140337c406be8add342b39e
MexicanOwl64 <55261799+MexicanOwl64@users.noreply.github.com>
2019-10-10 18:03:37 -0500

    Moving Block
    Gave a block gravity and made it move  up and down on the game

---
+++ silver-ball-lever Unity Project/Assets/MovingObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+
+    public float moveSpeed = 0.1f;
+    private Rigidbody2D rigidbody;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    /*use fixed update for physics code to be careful
+   * about how often we call expensive, hardcore intersive 
+   * physics stuff */
+
+    private void FixedUpdate()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        rigidbody.velocity = new Vector2(0, verticalInput);
+
+
+    }
+}
