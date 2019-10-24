43686bbc886566ec8fece0ab3b14ec7dd1b402c0
MexicanOwl64 <55261799+MexicanOwl64@users.noreply.github.com>
2019-10-24 18:07:21 -0500

    Added a Goal and WInning Text
    The goal was added and I was able to add text for the win.  I also fixed a movement issue I was having but still need to find a way to make it faster

---
+++ silver-ball-lever Unity Project/Assets/Scripts/Goal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Detect when the ball touches to goal to show a win message
+/// </summary>
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Touched Trigger");
+
+        if (collision.tag == "Player")
+        {
+            Debug.Log("You touched the goal");
+        }
+    }
+}
