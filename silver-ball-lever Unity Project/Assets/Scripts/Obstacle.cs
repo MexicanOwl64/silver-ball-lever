917a9aa889200186dd4a7422ec8a004a4b945339
MexicanOwl64 <55261799+MexicanOwl64@users.noreply.github.com>
2019-11-14 17:13:10 -0600

    Made an Obstacle
    Made an obstacle that makes the player lose the game.

---
+++ silver-ball-lever Unity Project/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+
+    public GameObject GameOverText;
+    private AudioSource audioSource;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Touched Trigger");
+
+        if (collision.tag == "Player")
+        {
+            GameOverText.SetActive(true); //tells us we win
+
+            audioSource.Play();
+
+            Debug.Log("You touched the bad object");
+        }
+    }
+}
